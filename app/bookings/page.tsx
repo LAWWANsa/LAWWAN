4dc25a15ed549ef0d9f4823fa8d3c9624ac154a9
LAWWANsa <moh.salman.j@gmail.com>
2026-09-14 15:53:38 +0300

    Delete app directory

--- app/bookings/page.tsx
+++
@@ -1,6 +0,0 @@
-import { redirect } from 'next/navigation'
-export default async function BookingsPage({searchParams}:{searchParams:Promise<{trainer?:string}>}){
- const {trainer}=await searchParams
- if(trainer) redirect(`/book/${trainer}`)
- redirect('/trainers')
-}
